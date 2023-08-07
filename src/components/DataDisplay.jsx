dfe89d869907515199e8176c48a789a0467486db
SAGH <42285767+8sagh8@users.noreply.github.com>
2023-08-07 11:38:29 -0400

    updated pages

--- src/components/DataDisplay.jsx
+++ src/components/DataDisplay.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const DataDisplay = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(process.env.PUBLIC_URL + '/data/mydata.json')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
   return (
     <div className="data-display">
-      Data will be displayed here.
+      {data.map(item => (
+        <div key={item.id}>
+          <h2>{item.subject}</h2>
+          <h3>References:</h3>
+          <p>{item.references.book}, {item.references.volume}, {item.references.page}, {item.references.line}</p>
+          <h3>Description:</h3>
+          <p>{item.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default DataDisplay;
+
