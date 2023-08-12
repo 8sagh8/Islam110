551df9cfcc85d483dd094c84dde7a90890141eac
SAGH <42285767+8sagh8@users.noreply.github.com>
2023-08-12 12:08:24 -0400

    updated icon image favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import logo from '../assets/logo.png'; // replace this with your actual logo file
+import logo from '../assets/logo.png'; // replace this with your actual logo file
 
 const Header = () => {
   return (
     <div className="header">
         <div className="logo">
-            {/* <img src={logo} alt="Logo" /> */}
-            <h1>Islamic Hub</h1>
+          <img src={logo} alt="Logo" /> 
+          <h1>Islamic Hub</h1>
         </div>
     </div>
 
