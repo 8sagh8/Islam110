683290f918039410cabd315722be9daf1bcba9d5
SAGH <42285767+8sagh8@users.noreply.github.com>
2023-09-02 04:44:20 -0400

    udpate

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const LeftMenu = () => {
   const zakiriBooksLink = "https://drive.google.com/drive/folders/13v2FOCE1jxoMFamo0qPzIgNuFPLqWayv"
   const sunniBooksLink = "https://drive.google.com/drive/u/1/my-drive"
+  const arabicBooksLink = "https://drive.google.com/drive/folders/1zUTIORX95hR6f6PIytimxnv5YKpOyat7"
   const youtubeLink = "https://www.youtube.com/"
     return (
       <div className="left-menu">
@@ -15,6 +16,9 @@ const LeftMenu = () => {
         </a>
         <a href={sunniBooksLink} target="_blank" rel="noopener noreferrer">
           Sunni Books
+        </a>
+        <a href={arabicBooksLink} target="_blank" rel="noopener noreferrer">
+          Arabic Grammar Books
         </a>
         <a href={youtubeLink} target="_blank" rel="noopener noreferrer">
           YouTube Link
