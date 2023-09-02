e1970002b6fc236b3ce06bb0881e8bf81e48aa7a
SAGH <42285767+8sagh8@users.noreply.github.com>
2023-09-02 03:05:41 -0400

    built persian page fully

--- src/components/FarsiDisplay.jsx
+++ src/components/FarsiDisplay.jsx
@@ -1,52 +1,100 @@
 import React, { useEffect, useState } from 'react';
+import '../css/FarsiDisplay.css'; // Import the CSS file for styling
 
 const FarsiDisplay = () => {
-  const [data, setData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [farsiData, setFarsiData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    fetch(process.env.PUBLIC_URL + '/data/mydata.json')
+    fetch(process.env.PUBLIC_URL + '/data/myfarsi.json')
       .then(response => response.json())
-      .then(data => setData(data));
+      .then(data => {
+        const sortedData = data.sort((a, b) => a.mazdar.localeCompare(b.mazdar));
+        setFarsiData(sortedData);
+      });
   }, []);
 
-  // const filteredData = data.filter(item =>
-  //   item.subject.toLowerCase().includes(searchTerm.toLowerCase())
-  // );
+  const filteredData = farsiData.filter(item => {
+    const lowerCaseSearchQuery = searchQuery.toLowerCase();
+    const fieldsToSearch = [
+      item.past_I,
+      item.past_You,
+      item.past_It,
+      item.past_We,
+      item.past_You_2,
+      item.past_They,
+      item.present_I,
+      item.present_You,
+      item.present_It,
+      item.present_We,
+      item.present_You_2,
+      item.present_They,
+      item.mazdar
+    ];
 
-  const filteredData = data.filter(item => {
-    const subjectMatch = item.subject.toLowerCase().includes(searchTerm.toLowerCase());
-    const referenceBookMatch = item.references.book.toLowerCase().includes(searchTerm.toLowerCase());
-    const referenceVolumeMatch = item.references.volume.toLowerCase().includes(searchTerm.toLowerCase());
-    const referencePageMatch = item.references.page.toLowerCase().includes(searchTerm.toLowerCase());
-    const referenceLineMatch = item.references.line.toLowerCase().includes(searchTerm.toLowerCase());
-    const descriptionMatch = item.description.toLowerCase().includes(searchTerm.toLowerCase());
-  
-    return subjectMatch || referenceBookMatch || referenceVolumeMatch || referencePageMatch || referenceLineMatch || descriptionMatch;
+    return fieldsToSearch.some(field => field.toLowerCase().includes(lowerCaseSearchQuery));
   });
-  
 
-  
+
   return (
-    <div className="data-display">
-      <div className="search-container">
-        <input 
-        type="text" 
-        placeholder="Search Word"
-        onChange={event => setSearchTerm(event.target.value)}
+    <div className="farsi-container">
+      <div className="farsi-search">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
         />
       </div>
-    {filteredData.slice().reverse().map(item => (
-      <div key={item.id}>
-        <h2>{item.id}: {item.subject}</h2>
-        <h3>References:</h3>
-        <p>{item.references.book} - V: {item.references.volume}, page# {item.references.page}, {item.references.line}</p>
-        <h3>Description:</h3>
-        <p>{item.description}</p>
-      </div>
-    ))}
+      
+      <table className="farsi-table">
+        <thead>
+          <tr>
+            <th>Past Tense</th>
+            <th>Present Tense</th>
+            <th>Mazdar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((item, index) => (
+            <tr key={item.id}>
+              <td>
+                <td>
+                <p>{item.past_I}</p>
+                </td><td>
+                <p>{item.past_You}</p>
+                </td><td>
+                <p>{item.past_It}</p>
+                </td><td>
+                <p>{item.past_We}</p>
+                </td><td>
+                <p>{item.past_You_2}</p>
+                </td><td>
+                <p>{item.past_They}</p>
+                </td>
+              </td>
+              <td>
+                <td>
+                <p>{item.present_I}</p>
+                </td><td>
+                <p>{item.present_You}</p>
+                </td><td>
+                <p>{item.present_It}</p>
+                </td><td>
+                <p>{item.present_We}</p>
+                </td><td>
+                <p>{item.present_You_2}</p>
+                </td><td>
+                <p>{item.present_They}</p>
+                </td>
+              </td>
+              <td>{index + 1}) <br /><br />{item.mazdar}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default FarsiDisplay;
